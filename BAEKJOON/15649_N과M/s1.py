79032e081d93e3d50fc52fac3ad748c59c89d054
NanJaengE <wodud5381@gmail.com>
2022-06-09 23:50:26 +0900

    0609

--- BAEKJOON/15649_N과M/s1.py
+++ BAEKJOON/15649_N과M/s1.py
@@ -1,11 +1,22 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-def dfs(n, m):
 
+def dfs():
+
+    if len(res) == M:
+        print(' '.join(map(str, res)))
+        return
+
+    for i in range(1, N+1):
+        if i not in res:
+            res.append(i)
+            dfs()
+            res.pop()
 
 
 T = int(input())
 for test in range(T):
     N, M = map(int, input().split())
-    dfs(N, M)
+    res = []
+    dfs()
